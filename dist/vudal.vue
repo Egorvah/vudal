e1dd460b95438df45e71832bd5e7d3e7b9554c9f
Stas Karpov <staskjs@mail.ru>
2017-12-12 15:50:50 +0300

    Add built files

--- dist/vudal.vue
+++ dist/vudal.vue
@@ -234,4 +234,48 @@
     background-color: #f0ad4e;
     border-color: #f0ad4e;
   }
+
+  .vudal-dimmer, .vudal-fade-in {
+
+    -webkit-animation: fadein .4s; /* Safari, Chrome and Opera > 12.1 */
+    -moz-animation: fadein .4s; /* Firefox < 16 */
+    -ms-animation: fadein .4s; /* Internet Explorer */
+    -o-animation: fadein .4s; /* Opera < 12.1 */
+    animation: fadein .4s;
+  }
+
+  @keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  /* Firefox < 16 */
+  @-moz-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  /* Safari, Chrome and Opera > 12.1 */
+  @-webkit-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  /* Internet Explorer */
+  @-ms-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  /* Opera < 12.1 */
+  @-o-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+  }
+
+  @keyframes fadeout {
+    from { opacity: 1; }
+    to   { opacity: 0; }
+  }
+
 </style>
